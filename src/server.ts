b6a993a9cea64d0bc775fa4caade662301bf07ec
Matheus França <matheus.franca@opovo.com.br>
2024-09-19 09:26:04 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import presentations from "./routes/get-presentations";
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(presentations);
 
